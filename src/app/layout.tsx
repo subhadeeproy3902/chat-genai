1eff0b7aeae209c53faedc0895b50e0a379679a8
Subhadeep Roy <subha9.5roy350@gmail.com>
2024-06-23 13:31:38 +0530

    fixed title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Chat Geminai",
+  title: "Chat Genai",
   description: "Introducing modern chatbot technology with Gemini AI.",
   openGraph: {
     images: [
